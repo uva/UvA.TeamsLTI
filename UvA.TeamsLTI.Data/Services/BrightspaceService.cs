5b172c0ddb84f40a484236e6a866f7cbf565f9fe
Gerrit Oomens <g.oomens@uva.nl>
2022-02-14 21:10:19 +0100

    Fix issue with deleted sections and groups

--- UvA.TeamsLTI.Data/Services/BrightspaceService.cs
+++ UvA.TeamsLTI.Data/Services/BrightspaceService.cs
@@ -72,11 +72,11 @@ namespace UvA.TeamsLTI.Services
                 case ContextType.Course:
                     return users;
                 case ContextType.Section:
-                    var sec = crs.Sections.First(s => s.SectionId == context.Id);
-                    return users.Where(u => u.IsTeacher || sec.Enrollments.Contains(u.Id));
+                    var sec = crs.Sections.FirstOrDefault(s => s.SectionId == context.Id);
+                    return users.Where(u => u.IsTeacher || sec?.Enrollments.Contains(u.Id) == true);
                 case ContextType.Group:
-                    var group = crs.GroupCategories.First(c => c.GroupCategoryId == context.GroupSetId).Groups.First(g => g.GroupId == context.Id);
-                    return users.Where(u => u.IsTeacher || group.Enrollments.Contains(u.Id));
+                    var group = crs.GroupCategories.FirstOrDefault(c => c.GroupCategoryId == context.GroupSetId)?.Groups.FirstOrDefault(g => g.GroupId == context.Id);
+                    return users.Where(u => u.IsTeacher || group?.Enrollments.Contains(u.Id) == true);
             }
             throw new ArgumentException("Invalid ContextType");
         }
