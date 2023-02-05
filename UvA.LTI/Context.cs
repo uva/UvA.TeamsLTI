e228affe0fd2311220c0c41432bf79cc6352320a
Gerrit Oomens <g.oomens@uva.nl>
2023-02-05 19:28:36 +0100

    Move LTI library to separate package

--- UvA.LTI/Context.cs
+++
@@ -1,9 +0,0 @@
-namespace UvA.LTI;
-
-public class Context
-{
-    public string Id { get; set; }
-    public string Title { get; set; }
-    public string Label { get; set; }
-    public string[] Type { get; set; }
-}
--- UvA.LTI/LtiClaimTypes.cs
+++
@@ -1,8 +0,0 @@
-namespace UvA.LTI;
-
-public static class LtiClaimTypes
-{
-    public const string Roles = "https://purl.imsglobal.org/spec/lti/claim/roles";
-    public const string Context = "https://purl.imsglobal.org/spec/lti/claim/context";
-    public const string Custom = "https://purl.imsglobal.org/spec/lti/claim/custom";
-}
